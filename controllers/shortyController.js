bd513359cab889e3a060dd37639c293eadae6237
Jeremy Hurley <hurleyjer@yahoo.com>
2016-08-19 13:17:15 -0400

    working host url

--- controllers/shortyController.js
+++ controllers/shortyController.js
@@ -3,7 +3,7 @@ var url = require ('url');
 module.exports = function(app){
 
 app.get('/', function(req, res){
-  var location = url.parse(req.url).href;
+  var location = req.protocol + '://' + req.get('host') + req.originalUrl;
   res.render('shorty.ejs', {location: location});
 })
 
