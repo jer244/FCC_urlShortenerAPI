181dff9234a5160f15ef99b8d052c867898857d8
Jeremy Hurley <hurleyjer@yahoo.com>
2016-08-19 13:07:03 -0400

    with url.href

--- controllers/shortyController.js
+++ controllers/shortyController.js
@@ -3,7 +3,7 @@ var url = require ('url');
 module.exports = function(app){
 
 app.get('/', function(req, res){
-  var location = req.url;
+  var location = url.parse(req.url).href;
   res.render('shorty.ejs', {location: location});
 })
 
