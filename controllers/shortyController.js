4e776045007e166eea5175eec0dadf39082dd0d1
Jeremy Hurley <hurleyjer@yahoo.com>
2016-08-19 12:07:41 -0400

    ejs

--- controllers/shortyController.js
+++ controllers/shortyController.js
@@ -1,7 +1,7 @@
 var url = require ('url');
 
 module.exports = function(app){
-var location = url.port;
+var location = url.host;
 
 app.get('/', function(req, res){
   res.render('shorty.ejs', {location: location});
