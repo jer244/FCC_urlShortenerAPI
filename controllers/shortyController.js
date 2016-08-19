3715eb5eaa127b7a9988d3b6432c04a7e57d3db2
Jeremy <hurleyjer@yahoo.com>
2016-08-19 12:13:21 -0400

    Update shortyController.js

--- controllers/shortyController.js
+++ controllers/shortyController.js
@@ -1,7 +1,7 @@
 var url = require ('url');
 
 module.exports = function(app){
-var location = url.host;
+var location = url.href;
 
 app.get('/', function(req, res){
   res.render('shorty.ejs', {location: location});
