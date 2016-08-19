9a3589e24fb5c1173ab4e5367a3261086eddb611
Jeremy <hurleyjer@yahoo.com>
2016-08-19 12:46:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.use(express.static('./public'));
 //fire controller
 shortyController(app);
 
-//listen to port 3000
-app.listen(3000);
-console.log('You are listening to port 3000');
+//listen to port 3000 (local)
+//app.listen(3000);
+//console.log('You are listening to port 3000');
+
+//for heroku
+app.listen(process.env.PORT, function(){
+console.log('Listening to default PORT')
+});
